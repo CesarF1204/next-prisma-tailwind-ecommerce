269fb23f90bdc652c1914af84ab7590089638eeb
Cesar Francisco <princexcesar@gmail.com>
2025-10-15 17:58:23 +0800

    feat/added cross sell products display in cart page

--- apps/storefront/src/app/(store)/(routes)/cart/components/grid.tsx
+++ apps/storefront/src/app/(store)/(routes)/cart/components/grid.tsx
@@ -7,6 +7,8 @@ import { useCartContext } from '@/state/Cart'
 import { Item } from './item'
 import { Receipt } from './receipt'
 import { Skeleton } from './skeleton'
+import { Separator } from '@/components/ui/separator'
+import { CrossSellProducts } from '../../products/[productId]/components/cross_sell_product'
 
 export const CartGrid = () => {
    const { loading, cart, refreshCart, dispatchCart } = useCartContext()
@@ -26,18 +28,44 @@ export const CartGrid = () => {
       )
    }
 
+   const crossSellProductsMerge = () => {
+      const mergedCrossSellProducts = new Map();
+
+      if (isVariableValid(cart?.items) && cart.items.length) {
+         for (const item of cart.items) {
+            const crossSellProducts = item?.product?.crossSellOf;
+            if (Array.isArray(crossSellProducts)) {
+               for (const product of crossSellProducts) {
+                  if (product?.id && !mergedCrossSellProducts.has(product.id)) {
+                     mergedCrossSellProducts.set(product.id, product);
+                  }
+               }
+            }
+         }
+      }
+
+      return Array.from(mergedCrossSellProducts.values());
+   };
+
    return (
-      <div className="mb-4 grid grid-cols-1 md:grid-cols-3 gap-3">
-         <div className="md:col-span-2">
-            {isVariableValid(cart?.items)
-               ? cart?.items?.map((cartItem, index) => (
-                    <Item cartItem={cartItem} key={index} />
-                 ))
-               : [...Array(5)].map((cartItem, index) => (
-                    <Skeleton key={index} />
-                 ))}
+      <>
+         <div className="mb-4 grid grid-cols-1 md:grid-cols-3 gap-3">
+            <div className="md:col-span-2">
+               {isVariableValid(cart?.items)
+                  ? cart?.items?.map((cartItem, index) => (
+                     <Item cartItem={cartItem} key={index} />
+                  ))
+                  : [...Array(5)].map((cartItem, index) => (
+                     <Skeleton key={index} />
+                  ))}
+            </div>
+            <Receipt />
          </div>
-         <Receipt />
-      </div>
+         <Separator />
+         <div>
+            <h2 className="my-4 text-xl font-medium tracking-tight">You might also like:</h2>
+            <CrossSellProducts products={crossSellProductsMerge()} />
+         </div>
+      </>
    )
 }
